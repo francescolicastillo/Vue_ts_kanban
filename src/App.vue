fb76f8117be259775aa0b02370772ddfba6c5bc2
Francesco <frank_hdz16@hotmail.com>
2022-04-07 00:00:00 +1510

    create methods

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Header />
+  <router-view />
+</template>
+
+<script setup lang="ts">
+import Header from './components/Header.vue';
+
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Gill Sans", sans-serif;
+}
+</style>
