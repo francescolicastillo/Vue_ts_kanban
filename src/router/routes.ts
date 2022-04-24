9b9cacbb57f9c18aa45ef8625750e5b9a1233b2c
Francesco <frank_hdz16@hotmail.com>
2022-04-25 00:00:00 +1510

    add router

---
+++ src/router/routes.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Home from "../pages/Home.vue";
+import Board from "../pages/Board.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home,
+    },
+    {
+      path: "/board/:id",
+      name: "Board",
+      component: Board,
+    },
+  ],
+});
+
+export default router;
